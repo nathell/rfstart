0b56150006fc9a2200bc4556ed29cccb025d9eb8
Daniel Janus <dj@danieljanus.pl>
2017-02-05 12:24:10 +0100

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject rfstart "0.1.0"
+  :description "A starting point for re-frame based projects."
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [re-frame "0.9.1"]
+                 [org.clojure/clojurescript "1.9.456" :scope "provided"]
+                 [reagent "0.6.0"]]
+  :plugins [[lein-cljsbuild "1.1.5"]]
+  :clean-targets ^{:protect false}
+    [:target-path
+     [:cljsbuild :builds :app :compiler :output-dir]
+     [:cljsbuild :builds :app :compiler :output-to]]
+  :source-paths ["src/clj"]
+  :resource-paths ["resources" "target/cljsbuild" "target/resources"]
+  :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
+                             :compiler {:output-to "target/cljsbuild/public/js/app.js"
+                                        :output-dir "target/cljsbuild/public/js/out"
+                                        :main "rfstart.core"
+                                        :asset-path "js/out"
+                                        :optimizations :none
+                                        :pretty-print true}}}})
